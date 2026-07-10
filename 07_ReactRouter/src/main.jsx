f730596758248426ad72ede8f81f806dfc8ad096
Sudhanshu-Soni15 <sudhanshusoni2002@gmail.com>
2026-07-10 22:09:33 +0530

    added home component

--- 07_ReactRouter/src/main.jsx
+++ 07_ReactRouter/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import Layout from "./Layout.jsx";
+import { Children } from "react";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
