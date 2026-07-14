6db10d7b906dc9a0efc56bb4c64816fbfb6d5ffb
Sudhanshu-Soni15 <sudhanshusoni2002@gmail.com>
2026-07-14 22:17:35 +0530

    added new context

--- 09_ThemeSwitcher/src/App.jsx
+++ 09_ThemeSwitcher/src/App.jsx
@@ -9,7 +9,13 @@ function App() {
 
   return (
     <>
-      <div className="bg-red text-yellow-700">Hello, World!</div>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+
+          <div className="w-full max-w-sm mx-auto"></div>
+        </div>
+      </div>
     </>
   );
 }
