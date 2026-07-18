763c87753b32aeb9127a636b5d08137c2e63afb8
Sudhanshu-Soni15 <sudhanshusoni2002@gmail.com>
2026-07-18 21:17:23 +0530

    project completed

--- 09_ThemeSwitcher/src/App.jsx
+++ 09_ThemeSwitcher/src/App.jsx
@@ -4,9 +4,11 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
-  const [ThemeMode, setThemeMode] = useState("light");
+  const [themeMode, setThemeMode] = useState("light");
 
   const lightTheme = () => {
     setThemeMode("light");
@@ -26,9 +28,13 @@ function App() {
       <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
         <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
 
-            <div className="w-full max-w-sm mx-auto"></div>
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
           </div>
         </div>
       </ThemeProvider>
