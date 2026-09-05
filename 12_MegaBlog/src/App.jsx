f6e88e0b767abd5c23901bf14e24e1e49e380a42
Sudhanshu-Soni15 <sudhanshusoni2002@gmail.com>
2026-09-05 19:15:25 +0530

    updatd App.jsx

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -2,10 +2,24 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((user) => {
+        if (user) {
+          dispatch(login({ userData: user }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <>
